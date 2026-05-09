0920dc74a8ac40a6a6f6ab17d023d353848357e1
Sebastian Andre Salas Córdova <sebastian.salas@tecsup.edu.pe>
2026-05-09 15:29:46 -0500

    Add API endpoints for Clientes and Pedidos

--- orderflow_api/urls.py
+++ orderflow_api/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for orderflow_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    # Endpoints Clientes
+    path('api/clientes/', views.ClienteListCreateView.as_view(), name='clientes-list-create'),
+    path('api/clientes/<int:pk>/', views.ClienteDetailView.as_view(), name='clientes-detail'),
+
+    # Endpoints Pedidos
+    path('api/pedidos/', views.PedidoListCreateView.as_view(), name='pedidos-list-create'),
+    path('api/pedidos/<int:pk>/', views.PedidoDetailView.as_view(), name='pedidos-detail'),
+
     path('admin/', admin.site.urls),
 ]
